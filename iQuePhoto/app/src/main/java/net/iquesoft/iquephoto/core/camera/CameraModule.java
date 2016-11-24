01e36b551388fc9b64fc905efe42ba7f1ba1a9c1
ktairov <pyro.sun@gmail.com>
2016-11-24 19:44:59 +0200

    Added moving Radial Tilt Shift.

---
+++ iQuePhoto/app/src/main/java/net/iquesoft/iquephoto/core/camera/CameraModule.java
@@ -0,0 +1,8 @@
+package net.iquesoft.iquephoto.core.camera;
+
+/**
+ * Created by Sergey on 11/24/2016.
+ */
+
+public class CameraModule {
+}
---
+++ iQuePhoto/app/src/main/java/net/iquesoft/iquephoto/util/MotionEventUtil.java
@@ -0,0 +1,8 @@
+package net.iquesoft.iquephoto.util;
+
+/**
+ * Created by Sergey on 11/24/2016.
+ */
+
+public class MotionEventUtil {
+}
